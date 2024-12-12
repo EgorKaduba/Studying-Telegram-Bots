703940db5fa3bc67ef81c9cda4e055d0ce19d364
egork <egor.kaduba@mail.ru>
2024-12-12 21:56:06 +0300

    Эхо-бот

---
+++ echo bot/echo_bot.py
@@ -0,0 +1,27 @@
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
+
+BOT_TOKEN = "1111111111:AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+
+
+@dp.message(Command(commands=['start']))
+async def start(message: Message):
+    await message.answer("Привет!\nМеня зовут Егор\nНапиши мне что-нибудь")
+
+
+@dp.message(Command(commands=['help']))
+async def bot_help(message: Message):
+    await message.answer("Напиши мне что-нибудь и в ответ я пришлю тебе твоё сообщение")
+
+
+@dp.message()
+async def send_echo(message: Message):
+    await message.reply(message.text)
+
+
+if __name__ == "__main__":
+    dp.run_polling(bot)
